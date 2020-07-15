d6e72521a46ac8ad21961752ea8f28ce32e32efc
Herman Cheung <herman.cheung@loblaw.ca>
2020-07-15 11:06:57 -0400

    add master setup

---
+++ master/setup.sh
@@ -0,0 +1,12 @@
+sudo nano /proc/sys/net/ipv4/ip_forward
+(Change from 0 to 1) 
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
